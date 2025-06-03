4d6d0ced754ce04a73e902886a134515eae2ba0d
Alperen Can <alperencan2020@gmail.com>
2025-06-03 22:56:43 +0300

    webui without submodule

---
+++ ETicaret.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+using System.Diagnostics;
+using Eticaret.Data;
+using ETicaret.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ETicaret.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly DatabaseContext _context;
+
+        public HomeController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = new HomePageViewModel
+            {
+                Sliders = await _context.Sliders.ToListAsync(),
+                News = await _context.News.ToListAsync(),
+                Products = await _context.Products.ToListAsync(),
+            };
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        } public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

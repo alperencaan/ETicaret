1e2bd8c5febb9ff64a6dd324e445793aedc15d87
Alperen Can <alperencan2020@gmail.com>
2025-06-08 04:02:13 +0300

    create loginviewmodel

---
+++ ETicaret.WebUI/Models/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ETicaret.WebUI.Models
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        [Display(Name = "Şifre")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }// kullanıcı yönlendir
+        public bool RememberMe { get; set; } // kullanıcıyı hatırla
+    }
+}

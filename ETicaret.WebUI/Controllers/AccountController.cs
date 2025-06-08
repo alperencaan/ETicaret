513c4ec292046eab51ab6f107e44f55a0add55a0
Alperen Can <alperencan2020@gmail.com>
2025-06-08 03:36:09 +0300

    update account

--- ETicaret.WebUI/Controllers/AccountController.cs
+++ ETicaret.WebUI/Controllers/AccountController.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Eticaret.Core.Entities;
+using Eticaret.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ETicaret.WebUI.Controllers
 {
     public class AccountController : Controller
     {
+
+        private readonly DatabaseContext _context;
+
+        public AccountController(DatabaseContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
@@ -12,9 +21,30 @@ namespace ETicaret.WebUI.Controllers
         {
             return View();
         }
+        [HttpPost]
+
+        public IActionResult SignIn(AppUser appUser)
+        {
+            return View();
+        }
         public IActionResult SignUp()
         {
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> SignUpAsync(AppUser appUser)
+        {
+            if (ModelState.IsValid)
+            {
+                await _context.AddAsync(appUser);  // AddAsync kullanın
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            // Eğer SignUp.cshtml varsa bunu dönmeniz daha mantıklı:
+            return View("SignUp", appUser);
+        }
+
     }
 }
+    
